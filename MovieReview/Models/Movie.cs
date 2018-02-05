df0d5b5bf948bdb5b0fcdc337d12bad43a789847
eimutisgenys <eimutis.genys@gmail.com>
2018-02-05 17:51:54 +0000

    Movie - User Tbl Classes

---
+++ MovieReview/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MovieReview.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Imdb ID")]
+        public int ImdbID { get; set; }
+
+        [Display(Name = "Movie Title")]
+        public int Title { get; set; }
+
+        [Display(Name = "Movie Year")]
+        public int Year { get; set; }
+    }
+}
---
+++ MovieReview/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MovieReview.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "User ID")]
+        public int UserID { get; set; }
+
+        [Display(Name = "First Name")]
+        public int FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public int LastName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public int DOB { get; set; }
+
+        [Display(Name = "Email")]
+        public int Email { get; set; }
+    }
+}
